8b3fee9318db76d721f7ae405647d8f5de0ea0b4
Brad White <white.bradleyj@gmail.com>
2016-10-06 02:16:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,37 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * (100 - 0)) + 0;
+  var obj = {item: price};
+  cart.push(obj);
+  console.log(`${item} has been added to your cart.`)
+  return cart
+  }
+
+
+  function viewCart(){
+  var cartKeys = Object.keys(cart)
+  if(cart.length == 0) {
+    console.log('Your shopping cart is empty.')
+  }
+  else {
+    var text = 'In your cart, you have'
+    for(var i=0; i<cart.length; i++){
+      var item = Object.keys(cart[i])
+      if(i<cart.length-1) {
+        text += ` ${item[0]} at $${cart[i][item[0]]},`
+      }
+      else {
+        text += ` ${item[0]} at $${cart[i][item[0]]}.`
+      }
+    }
+    console.log(text)
+  }
 }
